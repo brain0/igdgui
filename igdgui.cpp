79c60b378d9c6565b2493bb170fd657b2c72be71
Thomas Bächler <thomas@archlinux.org>
2011-10-30 00:07:05 +0200

    Add check boxes for auto rescan and auto refresh

--- igdgui.cpp
+++ igdgui.cpp
@@ -4,6 +4,9 @@
 #include "addportmapping.h"
 #include "portmappingdetails.h"
 
+#define AUTO_RESCAN_MIN_TIMEOUT 60
+#define AUTO_REFRESH_MIN_TIMEOUT 5
+
 IgdGuiApp::IgdGuiApp(QWidget *parent) : QMainWindow(parent) {
   setupUi(this);
   igd = new IGDDevice(this);
@@ -20,6 +23,20 @@ IgdGuiApp::IgdGuiApp(QWidget *parent) : QMainWindow(parent) {
   connect( buttonDetails, SIGNAL( clicked() ), this, SLOT( ShowSelectedPortMappingDetails() ) );
 
   forwardList->setModel(igd->getForwardDataModel());
+
+  rescanTimer = new QTimer(this);
+  refreshTimer = new QTimer(this);
+  connect( rescanTimer, SIGNAL( timeout() ), igd, SLOT( scan() ) );
+  connect( refreshTimer, SIGNAL( timeout() ), igd, SLOT( refresh() ) );
+  connect( checkAutoRescan, SIGNAL( stateChanged(int) ), this, SLOT( changeAutoRescanState(int) ) );
+  connect( checkAutoRefresh, SIGNAL( stateChanged(int) ), this, SLOT( changeAutoRefreshState(int) ) );
+  connect( editAutoRescanTimeout, SIGNAL( editingFinished() ), this, SLOT( changeAutoRescanTimeout() ) );
+  connect( editAutoRefreshTimeout, SIGNAL( editingFinished() ), this, SLOT( changeAutoRefreshTimeout() ) );
+}
+
+IgdGuiApp::~IgdGuiApp() {
+  rescanTimer->stop();
+  refreshTimer->stop();
 }
 
 void IgdGuiApp::SetScanWaitStatus() {
@@ -79,4 +96,41 @@ void IgdGuiApp::ShowSelectedPortMappingDetails() {
     d->raise();
     d->activateWindow();
   }
+}
+
+void IgdGuiApp::changeAutoRescanState(int state) {
+  if(state == Qt::Checked) {
+    editAutoRescanTimeout->setEnabled(true);
+    rescanTimer->start(getValue(editAutoRescanTimeout, AUTO_RESCAN_MIN_TIMEOUT)*1000);
+  } else if(state == Qt::Unchecked) {
+    editAutoRescanTimeout->setEnabled(false);
+  }
+}
+
+void IgdGuiApp::changeAutoRefreshState(int state) {
+  if(state == Qt::Checked) {
+    editAutoRefreshTimeout->setEnabled(true);
+    refreshTimer->start(getValue(editAutoRefreshTimeout, AUTO_REFRESH_MIN_TIMEOUT)*1000);
+  } else if(state == Qt::Unchecked) {
+    editAutoRefreshTimeout->setEnabled(false);
+  }
+}
+
+void IgdGuiApp::changeAutoRescanTimeout() {
+  rescanTimer->setInterval(getValue(editAutoRescanTimeout, AUTO_RESCAN_MIN_TIMEOUT)*1000);
+}
+
+void IgdGuiApp::changeAutoRefreshTimeout() {
+  refreshTimer->setInterval(getValue(editAutoRefreshTimeout, AUTO_REFRESH_MIN_TIMEOUT)*1000);
+}
+
+int IgdGuiApp::getValue(QLineEdit *e, int min) {
+  int ret;
+
+  ret = e->text().toInt();
+  if(ret < min) {
+    ret = min;
+    e->setText(QString::number(min));
+  }
+  return ret;
 }
--- igdgui.h
+++ igdgui.h
@@ -3,22 +3,30 @@
  
 #include "ui_igdgui.h"
 #include "igddevice.h"
+#include <QTimer>
 
 class IgdGuiApp : public QMainWindow, private Ui::igdGuiMainUi {
   Q_OBJECT
 
 public:
   IgdGuiApp(QWidget *parent = 0);
+  ~IgdGuiApp();
 private slots:
   void SetScanWaitStatus();
   void SetScanFinishedStatus();
   void DeleteSelectedPortMapping();
   void ShowSelectedPortMappingDetails();
+  void changeAutoRescanState(int);
+  void changeAutoRefreshState(int);
+  void changeAutoRescanTimeout();
+  void changeAutoRefreshTimeout();
 public slots:
   void ReadIGDData();
   void ShowAddPortMappingDialog();
 private:
   IGDDevice *igd;
+  QTimer *rescanTimer, *refreshTimer;
+  int getValue(QLineEdit *e, int min);
 };
 
 #endif
